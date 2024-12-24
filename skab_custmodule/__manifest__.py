904aecd7e0223b8da52c06b6d9bb78f599fa07c6
nagwagabr-rwps <pythonrealworldproblemsolution@gmail.com>
2024-12-24 18:21:39 +0000

    __manifest__.py

--- skab_custmodule/__manifest__.py
+++ skab_custmodule/__manifest__.py
@@ -1,34 +1,40 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "skab_custmodule",
+    'name': "Skab.custmodule",
+    
+    'summary': " انشاء مجموعات خاصة لشركات المقاولات وتخصيص صلاحيات مرتبطة بطبيعة هذا النشاط",
+    
+    'description': 
+    """
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
+انشاء مجموعات خاصة لشركات المقاولات وتخصيص صلاحيات مرتبطة بطبيعة هذا النشاط
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
+    
+    'author': "Real World Problem Solution Techniques (RWPST)",
+    'website': "https://ads.rwpst.online/",
+    
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    'category': 'Field Service',
+    'version': '1.0',
+    
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','calendar'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+
+#        'security/skab_security_rwpst.xml',
+#        'security/ir.model.access.csv'
+
+
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+#        'demo/demo.xml',
     ],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
-
