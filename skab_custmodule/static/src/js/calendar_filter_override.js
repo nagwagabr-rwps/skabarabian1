9cfc139ce44f1bd8daf8f8e71ee6d74a648c9987
nagwagabr-rwps <pythonrealworldproblemsolution@gmail.com>
2024-12-25 09:08:18 +0000

    Add JavaScript override for hiding 'Everybody's calendars'and assign to user

--- skab_custmodule/static/src/js/calendar_filter_override.js
+++ skab_custmodule/static/src/js/calendar_filter_override.js
@@ -2,13 +2,13 @@
 
 import CalendarModel from 'calendar.CalendarModel';
 
-// Override the makeFilterAll method to restrict "Everybody's calendars" to specific group
+// Override the makeFilterAll method to restrict "Everybody's calendars" to managers
 CalendarModel.include({
     makeFilterAll(previousAllFilter, isUserOrPartner, sectionLabel) {
-        // Check if the user belongs to a specific group (e.g., "Owner" group)
-        const userHasAccess = this.env.session.user_has_group('skab_custmodule.group_calendar_owner');
+        // Check if the user belongs to the manager group
+        const userHasAccess = this.env.session.user_has_group('appointment.group_appointment_manager');
         
-        // If the user is not part of the group, do not show the filter
+        // If the user is not part of the manager group, do not show the filter
         if (!userHasAccess) {
             return null; // This disables the filter for unauthorized users
         }
